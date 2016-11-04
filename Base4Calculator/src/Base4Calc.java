853e8709bef6e7286998279be83840479dc1a277
linmaogeng <363577572@qq.com>
2016-11-04 11:16:15 -0400

    Update Base4Calc.java

--- Base4Calculator/src/Base4Calc.java
+++ Base4Calculator/src/Base4Calc.java
@@ -1,10 +1,31 @@
 
-public class Base4Calc {
+public class Base4Calc 
+{
 	private int result;
 	
 	Base4Calc() { result = 0; }
 	
 	void clear() { result = 0; }
+	
+	void addition (int first, int second)
+	{
+		result = first + second;
+	}
+	
+	void subtraction (int first, int second)
+	{
+		result = first - second;
+	}
+	
+	void division (int first, int second)
+	{
+		result = first / second;
+	}
+	
+	void multiplication (int first, int second)
+	{
+		result = first * second;
+	}
 
 	// What other methods will you need?
 }
